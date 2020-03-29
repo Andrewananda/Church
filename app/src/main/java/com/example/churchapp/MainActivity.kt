abb9524d75e1795fb8ec8d169d7bdd1e649b4fde
Andrew Ananda <andrewtiema@gmail.com>
2020-03-29 14:35:58 +0300

    fix/navigation

--- app/src/main/java/com/example/churchapp/MainActivity.kt
+++ app/src/main/java/com/example/churchapp/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.churchapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.churchapp.views.SermonFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottom_navigation.setOnNavigationItemSelectedListener { menuItem->
+            when(menuItem.itemId){
+                R.id.navigation_home -> {
+                    val homeFragment = SermonFragment()
+                    loadFragment(homeFragment)
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+
     }
+    private fun loadFragment(fragment: Fragment) {
+        // load fragment
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.navigation_home, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
 }
